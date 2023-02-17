2fab4918cf89a74590195b23224de759015423ee
Hany Medhat <hany.medhat24@gmail.com>
2023-02-17 22:32:14 +0200

    best sellert item part 3

--- lib/features/Splash/presentation/view_models/views/widgets/best_seller_list_view_item.dart
+++ lib/features/Splash/presentation/view_models/views/widgets/best_seller_list_view_item.dart
@@ -2,6 +2,8 @@ import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../../../constants.dart';
+
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({Key? key}) : super(key: key);
 
@@ -29,15 +31,30 @@ class BestSellerListViewItem extends StatelessWidget {
             width: 30,
           ),
           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 width: MediaQuery.of(context).size.width * .5,
-                child: const Text(
+                child: Text(
                   'Harry Potter and the Goblet of Fire',
                   maxLines: 2,
                   overflow: TextOverflow.ellipsis,
-                  style: Styles.textStyle20,
+                  style: Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
                 ),
+              ),
+              const SizedBox(
+                height: 3,
+              ),
+              const Text(
+                'J.K. Rowling',
+                style: Styles.textStyle14,
+              ),
+              const SizedBox(
+                height: 3,
+              ),
+              Text(
+                '19.99 €',
+                style: Styles.textStyle20.copyWith(fontWeight: FontWeight.bold),
               ),
             ],
           )
