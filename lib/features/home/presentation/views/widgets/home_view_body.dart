35acacb6f0bf37e48f3b220777cc55b8c5e49ddc
Hany Medhat <hany.medhat24@gmail.com>
2023-02-17 15:17:43 +0200

    add featured list view to home

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,9 @@
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'custom_app_bar.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({Key? key}) : super(key: key);
@@ -10,33 +13,24 @@ class HomeViewBody extends StatelessWidget {
     return Column(
       children: const [
         CustomAppBor(),
+        FeaturedBooksListView(),
       ],
     );
   }
 }
 
-class CustomAppBor extends StatelessWidget {
-  const CustomAppBor({Key? key}) : super(key: key);
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 40),
-      child: Row(
-        children: [
-          Image.asset(
-            AssetsData.logo,
-            height: 18,
-          ),
-          const Spacer(),
-          IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                FontAwesomeIcons.magnifyingGlass,
-                size: 24,
-              )),
-        ],
-      ),
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return const CustomListViewItem();
+          }),
     );
   }
 }
