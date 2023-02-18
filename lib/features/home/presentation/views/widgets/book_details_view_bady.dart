017d6fd6e83312d411587634f504e970f00eba24
Hany Medhat <hany.medhat24@gmail.com>
2023-02-18 20:35:53 +0200

    create books action

--- lib/features/home/presentation/views/widgets/book_details_view_bady.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_bady.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/Splash/presentation/view_models/views/widgets/book_rating.dart';
+import 'package:bookly_app/features/Splash/presentation/view_models/views/widgets/books_action.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
@@ -48,6 +49,10 @@ class BookDetailsViewBady extends StatelessWidget {
           const BookRating(
             mainAxisAlignment: MainAxisAlignment.center,
           ),
+          const SizedBox(
+            height: 37,
+          ),
+          const BooksAction(),
         ],
       ),
     );
