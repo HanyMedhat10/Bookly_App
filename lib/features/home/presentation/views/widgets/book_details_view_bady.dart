a0ed7f59406e52cf7d8c55eafb59ab6d34e2d189
Hany Medhat <hany.medhat24@gmail.com>
2023-02-18 12:10:42 +0200

    create book details view

---
+++ lib/features/home/presentation/views/widgets/book_details_view_bady.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BookDetailsViewBady extends StatelessWidget {
+  const BookDetailsViewBady({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
