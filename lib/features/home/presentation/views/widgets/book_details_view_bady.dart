7026073532b640ffeae6f62089a9fe68607d9c7d
Hany Medhat <hany.medhat24@gmail.com>
2023-02-19 17:36:55 +0200

    add custom scroll view

--- lib/features/home/presentation/views/widgets/book_details_view_bady.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_bady.dart
@@ -12,66 +12,144 @@ class BookDetailsViewBady extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        children: [
-          const SafeArea(
-            child: CustomBookDetailsAppBar(),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .2),
-            child: const CustomBookImage(),
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          const Text(
-            'The Jungle Book',
-            style: Styles.textStyle30,
-          ),
-          const SizedBox(
-            height: 4,
-          ),
-          Opacity(
-            opacity: .7,
-            child: Text(
-              'Rudyard Kipling',
-              style: Styles.textStyle18.copyWith(
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.w500,
-                // color: const Color(0xff707070),
-              ),
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              children: [
+                const SafeArea(
+                  child: CustomBookDetailsAppBar(),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width * .2),
+                  child: const CustomBookImage(),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                const Text(
+                  'The Jungle Book',
+                  style: Styles.textStyle30,
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Opacity(
+                  opacity: .7,
+                  child: Text(
+                    'Rudyard Kipling',
+                    style: Styles.textStyle18.copyWith(
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w500,
+                      // color: const Color(0xff707070),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 18,
+                ),
+                const BookRating(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                const SizedBox(
+                  height: 37,
+                ),
+                const BooksAction(),
+                const Expanded(
+                  child: SizedBox(
+                    height: 50,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'You can also like',
+                    style: Styles.textStyle14.copyWith(
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const SimilarBooksListView(),
+                const SizedBox(
+                  height: 40,
+                ),
+              ],
             ),
           ),
-          const SizedBox(
-            height: 18,
-          ),
-          const BookRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          const SizedBox(
-            height: 37,
-          ),
-          const BooksAction(),
-          const SizedBox(
-            height: 50,
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'You can also like',
-              style: Styles.textStyle14.copyWith(
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          const SimilarBooksListView(),
-        ],
-      ),
+        ),
+      ],
     );
+    // return Padding(
+    //   padding: const EdgeInsets.symmetric(horizontal: 30),
+    //   child: Column(
+    //     children: [
+    //       const SafeArea(
+    //         child: CustomBookDetailsAppBar(),
+    //       ),
+    //       Padding(
+    //         padding: EdgeInsets.symmetric(horizontal: width * .2),
+    //         child: const CustomBookImage(),
+    //       ),
+    //       const SizedBox(
+    //         height: 40,
+    //       ),
+    //       const Text(
+    //         'The Jungle Book',
+    //         style: Styles.textStyle30,
+    //       ),
+    //       const SizedBox(
+    //         height: 4,
+    //       ),
+    //       Opacity(
+    //         opacity: .7,
+    //         child: Text(
+    //           'Rudyard Kipling',
+    //           style: Styles.textStyle18.copyWith(
+    //             fontStyle: FontStyle.italic,
+    //             fontWeight: FontWeight.w500,
+    //             // color: const Color(0xff707070),
+    //           ),
+    //         ),
+    //       ),
+    //       const SizedBox(
+    //         height: 18,
+    //       ),
+    //       const BookRating(
+    //         mainAxisAlignment: MainAxisAlignment.center,
+    //       ),
+    //       const SizedBox(
+    //         height: 37,
+    //       ),
+    //       const BooksAction(),
+    //       const Expanded(
+    //         child: SizedBox(
+    //           height: 50,
+    //         ),
+    //       ),
+    //       Align(
+    //         alignment: Alignment.centerLeft,
+    //         child: Text(
+    //           'You can also like',
+    //           style: Styles.textStyle14.copyWith(
+    //             fontWeight: FontWeight.w600,
+    //           ),
+    //         ),
+    //       ),
+    //       const SizedBox(
+    //         height: 16,
+    //       ),
+    //       const SimilarBooksListView(),
+    //       const SizedBox(
+    //         height: 40,
+    //       ),
+    //     ],
+    //   ),
+    // );
   }
 }
