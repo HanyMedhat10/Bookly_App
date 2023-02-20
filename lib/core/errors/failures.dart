bbf0d9b6dd5c72c9c3f2a86a281f34da9592e5ff
Hany Medhat <hany.medhat24@gmail.com>
2023-02-20 15:54:10 +0200

    create Servcer failure from dio error

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,3 +1,47 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failure {}
+abstract class Failure {
+  final String errMessage;
+
+  const Failure(this.errMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errMessage);
+  factory ServerFailure.fromDioError(DioError dioError) {
+    switch (dioError.type) {
+      case DioErrorType.connectionTimeout:
+        return ServerFailure('Connection timeout with ApiServer');
+      case DioErrorType.sendTimeout:
+        return ServerFailure('Send timeout with ApiServer');
+      case DioErrorType.receiveTimeout:
+        return ServerFailure('Receive timeout with ApiServer');
+      case DioErrorType.badCertificate:
+        return ServerFailure('Bad Certificate');
+      case DioErrorType.badResponse:
+        return ServerFailure.fromRespense(
+            dioError.response!.statusCode, dioError.response!.data);
+      case DioErrorType.cancel:
+        return ServerFailure('Request to ApiServer was cancel');
+      case DioErrorType.connectionError:
+        if (dioError.message!.contains('SocketException')) {
+          return ServerFailure('No Interner Connection');
+        } else {
+          return ServerFailure('Unexpected Error , please try again');
+        }
+      case DioErrorType.unknown:
+        return ServerFailure('Opps There was an Error, Please try again');
+    }
+  }
+  factory ServerFailure.fromRespense(dynamic statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 402) {
+      return ServerFailure(response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure('Your request not found, Please try later!');
+    } else if (statusCode == 500) {
+      return ServerFailure('Internal SErver error , Please try later');
+    } else {
+      return ServerFailure('Opps There was an Error, Please try again');
+    }
+  }
+}
