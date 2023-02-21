b5fc340fb3bc95842260064e896fa89d39884397
Hany Medhat <hany.medhat24@gmail.com>
2023-02-21 05:30:22 +0200

    create newest books cubit state

---
+++ lib/features/home/presentation/manger/newset_books_cubit/newset_books_state.dart
@@ -0,0 +1,10 @@
+part of 'newset_books_cubit.dart';
+
+abstract class NewsetBooksState extends Equatable {
+  const NewsetBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class NewsetBooksInitial extends NewsetBooksState {}
