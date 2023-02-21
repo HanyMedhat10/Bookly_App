55373fb4519714bbce8d4bcceb830f1ea89454a6
Hany Medhat <hany.medhat24@gmail.com>
2023-02-21 07:02:03 +0200

    display featured books part 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:bookly_app/constants.dart';
-import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo_impl.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'core/utils/serive_locator.dart';
 import 'features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart';
@@ -26,7 +23,7 @@ class BooklyApp extends StatelessWidget {
         BlocProvider(
           create: (context) => FeaturedBooksCubit(
             getIt.get<HomeRepoImpl>(),
-          ),
+          )..fetchFeaturedBooks(),
         ),
         BlocProvider(
           create: (context) => NewsetBooksCubit(
